50bae49e1de8c2235d3dcae5c4623e1251d750a4
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-07-20 16:45:39 -0300

    Add carousel image functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
 const menuBtn = document.querySelector('.menu-btn')
 let menuOpen = false
+const controls = document.querySelectorAll('.control')
+let currentItem = 0
+const items = document.querySelectorAll('.item')
+const maxItems = items.length
 
 menuBtn.addEventListener('click', () => {
     if(!menuOpen) {
@@ -10,3 +14,48 @@ menuBtn.addEventListener('click', () => {
         menuOpen = false
     }
 })
+
+controls.forEach( control => {
+    control.addEventListener('click', () => {
+        const isLeft = control.classList.contains('left-arrow')
+        const galleryWrapper = document.querySelector('.gallery-wrapper')
+
+        if (isLeft) {
+            currentItem -= 1
+        } else {
+            currentItem += 1
+        }
+
+        if (currentItem >= maxItems) {
+            currentItem = 0
+        }
+
+        if (currentItem < 0) {
+            currentItem = maxItems - 1
+        }
+
+        // if (currentItem == (maxItems - 1)) {
+        //     galleryWrapper.style.borderRight = '1px solid #fff'
+        // } else {
+        //     galleryWrapper.style.borderRight = 'none'
+        // }
+
+        items.forEach(item => item.classList.remove('current-item'))
+
+        items[currentItem].scrollIntoView({
+            inline: 'center',
+            block: 'nearest',
+            behavior: 'smooth'
+        })
+
+        items[currentItem].classList.add('current-item')
+    })
+})
+
+// function carouselAnimate() {
+//     controls[1].click()
+// }
+
+// setInterval(() => {
+//     carouselAnimate()
+// }, 5000)
